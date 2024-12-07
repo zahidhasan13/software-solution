959841cbe5e84ec6b3bb0cff94819b392056da32
zahidhasan13 <zahidhasanofficial13@gmail.com>
2024-12-07 15:44:59 +0600

    navigation menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   FaBars,
   FaDribbble,
@@ -7,8 +8,14 @@ import {
   FaTwitter,
   FaYoutube,
 } from "react-icons/fa";
+import { FaX } from "react-icons/fa6";
 
 const Header = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  const handleMenu = () => {
+    setMenuToggle(!menuToggle);
+  };
   return (
     <header className="header shadow-md">
       <nav className="nav flex items-center justify-between py-5 container mx-auto px-5 xl:px-0">
@@ -37,23 +44,30 @@ const Header = () => {
             Software Solution
           </a>
         </div>
-        <div className="text-xl">
-          <button>
+        <div className="text-2xl">
+          <button onClick={handleMenu}>
             <FaBars />
           </button>
         </div>
-        <div className="hidden">
-          <ul className="links flex items-center gap-5">
-            {["home", "services", "products", "about us", "contact us"].map(
-              (link, index) => (
-                <li key={index}>
-                  <a href="#">{link}</a>
-                </li>
-              )
-            )}
-          </ul>
-        </div>
       </nav>
+      <div className={menuToggle ? "block" : "hidden"}>
+        <ul className="links flex flex-col gap-5 absolute right-0 top-0 w-full md:w-[500px] bg-[#fff/70] backdrop-blur h-screen shadow z-10 py-5 px-10">
+          <li className="text-right">
+            <button onClick={handleMenu} className="text-3xl">
+              <FaX />
+            </button>
+          </li>
+          {["home", "services", "products", "about us", "contact us"].map(
+            (link, index) => (
+              <li key={index}>
+                <a href="#" className="text-3xl uppercase">
+                  {link}
+                </a>
+              </li>
+            )
+          )}
+        </ul>
+      </div>
     </header>
   );
 };
