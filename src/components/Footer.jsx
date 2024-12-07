fcf031f843e79db1f0ffb74515bb93bd98de6161
zahidhasan13 <zahidhasanofficial13@gmail.com>
2024-12-07 15:23:50 +0600

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import {
+  FaDribbble,
+  FaFacebook,
+  FaLinkedin,
+  FaPinterest,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+const Footer = () => {
+  return (
+    <footer className="footer shadow-md">
+      <div className="flex flex-col items-center justify-center gap-3 md:gap-5 py-5 container mx-auto px-5 xl:px-0">
+        <div>
+          <a href="#" className="text-2xl md:text-3xl font-semibold uppercase">
+            software solution
+          </a>
+        </div>
+        <div className="flex items-center gap-3 text-lg md:text-xl">
+          <a href="#">
+            <FaFacebook />
+          </a>
+          <a href="#">
+            <FaLinkedin />
+          </a>
+          <a href="#">
+            <FaTwitter />
+          </a>
+          <a href="#">
+            <FaYoutube />
+          </a>
+          <a href="#">
+            <FaPinterest />
+          </a>
+          <a href="#">
+            <FaDribbble />
+          </a>
+        </div>
+        <p className="text-xs font-medium">
+          &copy; {new Date().getFullYear()} All right reserved by{" "}
+          <a href="#" className="text-orange-500 hover:underline">
+            Software Solution
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,11 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const Main = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
